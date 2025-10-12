0f91192481c42559622a059ae4e0b5940231a1d1
vinicbot-dev <devkelvin903@gmail.com>
2025-10-12 11:51:57 +0300

    Update config.js

--- setting/config.js
+++ setting/config.js
@@ -65,7 +65,7 @@ global.AI_CHAT = "true", // Set to "true" to enable AI chatbot by default
 
 // ========= Add modeStatus and versions ========= //
 global.modeStatus = "Public";
-global.versions = "1.3.4";
+global.versions = "1.3.5";
 
 // ========= Setting WM ========= //
 global.packname = 'Vinic';
