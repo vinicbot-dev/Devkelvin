073970d72f3c5e236107fe904f3d730f070a5eb2
vinicbot-dev <devkelvin903@gmail.com>
2025-12-30 12:04:06 +0300

    Update config.js

--- config.js
+++ config.js
@@ -63,7 +63,7 @@ global.AI_CHAT = "false", // Set to "true" to enable AI chatbot by default
 
 // ========= Add modeStatus and versions ========= //
 global.modeStatus = "Public";
-global.versions = "1.4.0";
+global.versions = "1.4.1";
 
 // ========= Setting WM ========= //
 global.packname = 'Jexploit';
