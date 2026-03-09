aa80c7f5448ca8e43e12f61d6f1bdb435d0d813d
vinicbot-dev <devkelvin903@gmail.com>
2026-03-09 07:56:54 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -65,7 +65,7 @@ global.AI_CHAT = "false"; // Set to "true" to enable AI chatbot by default
 
 // ========= Add modeStatus and versions ========= //
 global.modeStatus = "Public";
-global.versions = "1.4.7";
+global.versions = "1.4.6";
 
 // ========= Setting WM ========= //
 global.packname = 'Jexploit';
@@ -78,7 +78,6 @@ global.wwe = "https://www.wwe.com/api/news";
 global.wwe1 = "https://www.thesportsdb.com/api/v1/json/3/searchfilename.php?e=wwe";
 global.wwe2 = "https://www.thesportsdb.com/api/v1/json/3/searchevents.php?e=wrestling";
 global.falcon = "https://flowfalcon.dpdns.org";
-global.updateZipUrl = "https://github.com/vinicbot-dev/Devkelvin/archive/refs/heads/main.zip";
 
 global.gcount = {
   prem: 500,
