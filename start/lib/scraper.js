6c698a71b87d60198e2bbb7a2cb086d67d31615a
vinicbot-dev <devkelvin903@gmail.com>
2026-04-29 18:29:37 +0300

    Add files via upload

--- start/lib/scraper.js
+++ start/lib/scraper.js
@@ -26,38 +26,6 @@ function wallpaper(title, page = '1') {
     })
 }
 
-async function remini(imageBuffer, mode = "enhance") {
-  return new Promise(async (resolve, reject) => {
-    const validModes = ["enhance", "recolor", "dehaze"];
-    if (!validModes.includes(mode)) mode = "enhance";
-
-    const apiUrl = `https://inferenceengine.vyro.ai/${mode}`;
-    
-    try {
-      const formData = new FormData();
-      formData.append("model_version", 1);
-      formData.append("image", Buffer.from(imageBuffer), {
-        filename: "enhanced_image.jpg",
-        contentType: "image/jpeg",
-      });
-
-      const response = await axios.post(apiUrl, formData, {
-        headers: {
-          ...formData.getHeaders(),
-          "User-Agent": "okhttp/4.9.3",
-          Connection: "Keep-Alive",
-          "Accept-Encoding": "gzip",
-        },
-        responseType: "arraybuffer",
-      });
-
-      resolve(Buffer.from(response.data));
-    } catch (error) {
-      reject(`❌ Error processing image: ${error.message}`);
-    }
-  });
-}
-
 function wikimedia(title) {
     return new Promise((resolve, reject) => {
         axios.get(`https://commons.wikimedia.org/w/index.php?search=${title}&title=Special:MediaSearch&go=Go&type=image`)
@@ -104,4 +72,4 @@ function styletext(teks) {
     })
 }
 
-module.exports = { wallpaper, remini,wikimedia, ringtone, styletext }
+module.exports = { wallpaper, wikimedia, ringtone, styletext }
